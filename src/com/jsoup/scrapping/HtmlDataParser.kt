b5083aca09685a710d79bb07e199807c580fc004
mazharulsabbir <mazharul15-8950@diu.edu.bd>
2020-09-09 12:49:36 +0600

    parsing html to data

--- src/com/jsoup/scrapping/HtmlDataParser.kt
+++ src/com/jsoup/scrapping/HtmlDataParser.kt
@@ -23,6 +23,43 @@ const val SRC_CSS_QUERY = "img[src]~a[href],a[href]~img[src],a[href],[src]"
 /* inline level elements: <a><abbr><acronym><b><bdo><big><br><button><cite><code><dfn><em><i><img><input><kbd><label><map><object><output><q><samp><script><select><small><span><strong><sub><sup><textarea><time><tt><var>*/
 
 class HtmlDataParser(private val doc: Document) {
+    private val blockLevelElements = arrayOf(
+            "hr",
+            "table",
+            "video",
+            "ul",
+            "tfoot",
+            "section",
+            "li",
+            "main",
+            "nav",
+            "noscript",
+            "ol",
+            "p",
+            "pre",
+            "address",
+            "article",
+            "aside",
+            "blockquote",
+            "canvas",
+            "dd",
+            "div",
+            "dl",
+            "dt",
+            "fieldset",
+            "figcaption",
+            "figure",
+            "footer",
+            "form",
+            "h1",
+            "h2",
+            "h3",
+            "h4",
+            "h5",
+            "h6",
+            "header",
+            "br"
+    )
 
     private val imports = doc.select(LINKS_CSS_QUERY)
     private val urls = doc.select(SRC_CSS_QUERY)
@@ -32,35 +69,52 @@ class HtmlDataParser(private val doc: Document) {
 
         val elements = doc.getElementsByTag("body")
         if (elements.isNotEmpty()) {
+            // todo: use string builder to add strings for inline level elements and add line break for block level elements
+            val stringBuilder = StringBuilder()
+
             elements.forEach { element ->
                 if (element.childrenSize() > 0) {
                     element.allElements.forEach {
-                        it.select(LINKS_CSS_QUERY).first()?.let { element1 ->
-                            print(element1)
-                        }
-                        it.select(SRC_CSS_QUERY).first()?.let { element1 ->
-                            print(element1)
+                        if (blockLevelElements.contains(it.tagName())) {
+                            stringBuilder.append("\n")
                         }
 
-                        if(it.text().isNotEmpty()){
-                            println(it.text())
-                        }else{
-                            println(it.tagName())
+                        it.select(LINKS_CSS_QUERY).first()?.let { element1 ->
+                            stringBuilder.append(element1)
+                        }
+                        it.select(SRC_CSS_QUERY).first()?.let { element1 ->
+                            stringBuilder.append(element1)
+                        }
+
+                        if (it.text().isNotEmpty()) {
+                            stringBuilder.append(it.text())
+                        }
+
+                        if (blockLevelElements.contains(it.tagName())) {
+                            stringBuilder.append("\n")
                         }
                     }
                 } else {
+                    if (blockLevelElements.contains(element.tagName())) {
+                        stringBuilder.append("\n")
+                    }
+
                     element.select(LINKS_CSS_QUERY).first()?.let {
-                        print(it)
+                        stringBuilder.append(it)
                     }
                     element.select(SRC_CSS_QUERY).first()?.let {
-                        print(it)
+                        stringBuilder.append(it)
                     }
-                    if(element.text().isNotEmpty()){
-                        println(element.text())
-                    }else{
-                        println(element.tagName())
+                    if (element.text().isNotEmpty()) {
+                        stringBuilder.append(element.text())
+                    }
+
+                    if (blockLevelElements.contains(element.tagName())) {
+                        stringBuilder.append("\n")
                     }
                 }
+
+                println(stringBuilder.toString())
             }
         }
 
