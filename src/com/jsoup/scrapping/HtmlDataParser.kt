b225e04cf0672df9e5a5cfaaa36e47e7006e95bc
mazharulsabbir <mazharul15-8950@diu.edu.bd>
2020-09-13 14:27:14 +0600

    parsing html to data

--- src/com/jsoup/scrapping/HtmlDataParser.kt
+++ src/com/jsoup/scrapping/HtmlDataParser.kt
@@ -10,6 +10,7 @@ import org.jsoup.nodes.Element
 import java.util.*
 import java.util.function.Consumer
 import kotlin.collections.HashMap
+import kotlin.collections.HashSet
 
 /**
  *
@@ -24,53 +25,49 @@ class HtmlDataParser(private val doc: Document) {
         val resource = mutableListOf<Resource>()
 
         val elements = doc.getElementsByTag(BODY)
+        val data = mutableListOf<String>()
+
         if (elements.isNotEmpty()) {
             // todo: use string builder to add strings for inline level elements and add line break for block level elements
-            val stringBuilder = StringBuilder()
 
             elements.forEach { element ->
                 if (element.childrenSize() > 0) {
                     element.allElements.forEach {
-                        if (blockLevelElements.contains(it.tagName())) {
-                            stringBuilder.append("\n")
-                        }
-
                         it.select(LINKS_CSS_QUERY).first()?.let { element1 ->
-                            stringBuilder.append(element1)
+                            if (!data.contains(element1.toString())) data.add(element1.toString())
                         }
                         it.select(SRC_CSS_QUERY).first()?.let { element1 ->
-                            stringBuilder.append(element1)
+                            if (!data.contains(element1.toString())) data.add(element1.toString())
                         }
 
                         if (it.text().isNotEmpty()) {
-                            stringBuilder.append(it.text())
+                            if (!data.contains(it.text())) data.add(it.text())
                         }
 
                         if (blockLevelElements.contains(it.tagName())) {
-                            stringBuilder.append("\n")
-                        }else stringBuilder.append(" ")
+                            data.add("\n")
+                        } else data.add(" ")
+
                     }
                 } else {
-                    if (blockLevelElements.contains(element.tagName())) {
-                        stringBuilder.append("\n")
-                    }
-
                     element.select(LINKS_CSS_QUERY).first()?.let {
-                        stringBuilder.append(it)
+                        if (!data.contains(it.toString())) data.add(it.toString())
                     }
                     element.select(SRC_CSS_QUERY).first()?.let {
-                        stringBuilder.append(it)
+                        if (!data.contains(it.toString())) data.add(it.toString())
                     }
                     if (element.text().isNotEmpty()) {
-                        stringBuilder.append(element.text())
+                        if (!data.contains(element.text())) data.add(element.text())
                     }
 
                     if (blockLevelElements.contains(element.tagName())) {
-                        stringBuilder.append("\n")
-                    }else stringBuilder.append(" ")
+                        data.add("\n")
+                    } else data.add(" ")
                 }
+            }
 
-                println(stringBuilder.toString())
+            data.forEach {
+                print(it)
             }
         }
 
